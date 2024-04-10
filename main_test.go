05a3549b9ea2b9fc07ec5c742e600e639b3e7967
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-10 11:45:50 +0530

    Added subtests

--- main_test.go
+++ main_test.go
@@ -3,8 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T){
-    got := Hello("Chris")
-    want := "Hello, Chris"
+    // Writing sub-tests
+    // Test case 1
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Chris")
+        want := "Hello, Chris"
+
+        // if got != want {
+        //     t.Errorf("got %q want %q", got, want)
+        // }
+        assertCorrectMessage(t, got, want)
+    })
+    // Test case 2
+    t.Run("say, 'Hello, World' when an empty string is supplied", func(t *testing.T){
+        got := Hello("")
+        want := "Hello, World"
+
+        // if got != want {
+        //     t.Errorf("got %q want %q", got, want)
+        // }
+        assertCorrectMessage(t, got, want)
+    })
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+    t.Helper() // Tells the test suite that this method is a helper
+
+    // When a test case fails, then the test suite reports back the  
+    // line number of the line what is failing. Now, if we do not 
+    // specify t.Helper() then the lines that follow will be reported
+    // for error instead of the lines from the main() file as this will 
+    // be treated as any ordinary function and not a testing-helper func
 
     if got != want {
         t.Errorf("got %q want %q", got, want)
