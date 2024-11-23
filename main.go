bed77ba67679842c0b646d1f6069b9fd45559bb6
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-11-23 11:38:15 +0530

    Simple REPL added

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
-const englishHelloPrefix = "Hello, "
+var ErrNoPath = errors.New("Path not specified")
 
-func Hello(name string) string {
-    if name == "" {
-        name = "World"
-    }
-    return englishHelloPrefix + name
+func execInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+	args := strings.Split(input, " ")
+
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
 }
 
-func main()  {
-    fmt.Println(Hello("world"))
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("rizz-shell >>") // Prompts
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
 }
