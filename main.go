93c8053b1aa9baadffc6f426da2911ada1fa36ff
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-11-23 12:25:01 +0530

    Built-in shell commands handled

--- main.go
+++ main.go
@@ -15,10 +15,21 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 	args := strings.Split(input, " ")
 
+	// Switch case to handle shell built-ins
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return ErrNoPath
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+	// Prepare, setup and execute
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
-
 	return cmd.Run()
 }
 
